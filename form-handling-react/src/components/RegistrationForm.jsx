7f1e1588619ff57cc29dd539e46b66ccc6ecbddc
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-03-10 17:00:48 +0100

    Form handling

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -7,6 +7,8 @@ const RegistrationForm = () => {
     password: "",
   });
 
+  const { username, email, password } = formData; // Destructure values
+
   const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
@@ -19,9 +21,9 @@ const RegistrationForm = () => {
 
   const validate = () => {
     let newErrors = {};
-    if (!formData.username) newErrors.username = "Username is required";
-    if (!formData.email) newErrors.email = "Email is required";
-    if (!formData.password) newErrors.password = "Password is required";
+    if (!username) newErrors.username = "Username is required";
+    if (!email) newErrors.email = "Email is required";
+    if (!password) newErrors.password = "Password is required";
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
@@ -40,19 +42,19 @@ const RegistrationForm = () => {
 
       <div>
         <label>Username:</label>
-        <input type="text" name="username" value={formData.username} onChange={handleChange} />
+        <input type="text" name="username" value={username} onChange={handleChange} />
         {errors.username && <p style={{ color: "red" }}>{errors.username}</p>}
       </div>
 
       <div>
         <label>Email:</label>
-        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        <input type="email" name="email" value={email} onChange={handleChange} />
         {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
       </div>
 
       <div>
         <label>Password:</label>
-        <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        <input type="password" name="password" value={password} onChange={handleChange} />
         {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
       </div>
 
