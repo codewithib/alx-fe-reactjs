4210ec06b59e1b8218b4f6cce86b7f4869260203
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-03-10 15:03:21 +0100

    Created userCard profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 
 
@@ -31,6 +32,12 @@ function App() {
             <MainContent />
             <Footer />
         </div>
+
+        <div>
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        </div>
+
+        
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
