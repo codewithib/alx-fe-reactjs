c0dec3f39fe0c7266b79276b15bcc90a7f563fa7
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-03-10 17:57:21 +0100

    test

---
+++ react-todo/src/components/TodoList.test.jsx
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+
+test("renders TodoList component", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Todo List")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  const input = screen.getByPlaceholderText("Add a new todo");
+  const addButton = screen.getByText("Add Todo");
+
+  fireEvent.change(input, { target: { value: "Test Todo" } });
+  fireEvent.click(addButton);
+
+  expect(screen.getByText("Test Todo")).toBeInTheDocument();
+});
+
+test("toggles a todo completion", () => {
+  render(<TodoList />);
+  const todoItem = screen.getByText("Learn React");
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+  const deleteButton = screen.getAllByText("Delete")[0];
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+});
