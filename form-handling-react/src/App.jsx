8d84f9cf7ffbabe72c1b10f8a60dd7b1effc9716
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-03-10 17:12:36 +0100

    Form handling

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import FormikForm from './components/formikForm.js';
+import FormikForm from '../formikForm.js';
 
 function App() {
   return (
