9811588dda87384b99c8a868d26f3676695db39a
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-03-10 15:57:28 +0100

    Modify navbar to include display: flex

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "navy", color: "white", textAlign: "center" }}>
+    <nav style={{ padding: "10px", backgroundColor: "navy", color: "white", textAlign: "center", display: "flex", justifyContent: "space-between" }}>
       <Link to="/" style={{ margin: "10px", color: "white", textDecoration: "none" }}>Home</Link>
       <Link to="/about" style={{ margin: "10px", color: "white", textDecoration: "none" }}>About</Link>
       <Link to="/services" style={{ margin: "10px", color: "white", textDecoration: "none" }}>Services</Link>
